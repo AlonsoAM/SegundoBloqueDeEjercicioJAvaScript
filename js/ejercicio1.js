4b64a79ca1fe9a31fa89c9d5907d204d62654c4b
AlonsoAM <alonso.amoreno@live.com>
2020-04-07 13:28:00 -0500

    Ejercicio 1
    Primer ejercicio desarrollado, agregando ejercicio 2

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,1 +1,6 @@
-alert("Hola")
+const invertirCadena = (cadena = "") =>
+(!cadena) ?
+console.console.warn('No ingresaste cuna cadena de texto') :
+console.info(cadena.split('').reverse().join(''))
+
+invertirCadena("Hola Mundo")
